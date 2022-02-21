d83b1d15e484a7c59f6a05af69ba60a93b3ea08f
Naoko Nishimura <naoko.nishimura1018@gmail.com>
2022-02-21 20:09:14 +0900

    chore: remove response hijack

--- apps/sample-ui/src/mocks/handlers.ts
+++ apps/sample-ui/src/mocks/handlers.ts
@@ -8,26 +8,26 @@ export const handlers = [
   rest.delete(`${process.env.REACT_APP_API_URL}/*`, recordRestResponse),
 
   // custom interceptor
-  rest.post(`${process.env.REACT_APP_API_URL}/login`, async (req, res, ctx) => {
-    // Persist user's authentication in the session
-    sessionStorage.setItem('is-authenticated', 'true')
-    try {
-      const mock = require('./json/post-login.json')
-      return res(ctx.status(200), ctx.json(mock))
-    } catch (error) {
-      console.error(error)
-    }
-  }),
+  // rest.post(`${process.env.REACT_APP_API_URL}/login`, async (req, res, ctx) => {
+  //   // Persist user's authentication in the session
+  //   sessionStorage.setItem('is-authenticated', 'true')
+  //   try {
+  //     const mock = require('./json/post-login.json')
+  //     return res(ctx.status(200), ctx.json(mock))
+  //   } catch (error) {
+  //     console.error(error)
+  //   }
+  // }),
 
-  rest.get(
-    `${process.env.REACT_APP_API_URL}/magicitems`,
-    async (req, res, ctx) => {
-      try {
-        const mock = require('./json/get-magicitems.json')
-        return res(ctx.status(200), ctx.json(mock))
-      } catch (error) {
-        console.error(error)
-      }
-    }
-  ),
+  // rest.get(
+  //   `${process.env.REACT_APP_API_URL}/magicitems`,
+  //   async (req, res, ctx) => {
+  //     try {
+  //       const mock = require('./json/get-magicitems.json')
+  //       return res(ctx.status(200), ctx.json(mock))
+  //     } catch (error) {
+  //       console.error(error)
+  //     }
+  //   }
+  // ),
 ]
